4a9bb9c7dddad6da7dd7b6a7ce972d884dcd0bf4
Ankit <9933darkknight@gmail.com>
2023-10-01 21:36:10 +0530

    274: Added explanation

--- top-interview-150/leet-274-h-index.ts
+++ top-interview-150/leet-274-h-index.ts
@@ -37,3 +37,21 @@ function hIndex(citations: number[]): number {
 
   return h;
 }
+
+/*
+EXPLANATION:
+
+**Solution 1: Sorting**
+
+In this solution, we first sort the `citations` array in descending order. Then we iterate over the sorted array. The variable `h` is used to track the current index. As long as the citation count of the current paper is greater than `h`, we increment `h`. The loop continues until we find a paper with a citation count that is less than or equal to `h`, or until we've checked all papers.
+
+For example, if `citations = [3, 0, 6, 1, 5]`, after sorting we get `[6, 5, 3, 1, 0]`. We start with `h = 0`. The first paper has 6 citations which is greater than `h`, so we increment `h` to 1. We do the same for the next three papers and increment `h` to 3. The last paper has 0 citations which is not greater than `h`, so we stop and return `h = 3`.
+
+**Solution 2: Counting Sort**
+
+In this solution, we first create a new array `papers` where the index represents the citation count and the value at that index represents the number of papers with that citation count. For each citation in the `citations` array, we increment the corresponding value in the `papers` array. If a citation count is larger than the total number of papers, we increment the value at the last index of the `papers` array.
+
+Then we iterate over the `papers` array from end to start. We keep a running sum `s` of the values in the `papers` array. As long as `s` is less than the current index `h`, we decrement `h`. The loop continues until `s` is not less than `h`.
+
+For example, if `citations = [3, 0, 6, 1, 5]`, after counting sort we get `papers = [1, 1, 0, 1, 0, 2]`. We start with `h = 5` and `s = 2`. Since `s < h`, we decrement `h` to 4 and add `papers[4] = 0` to `s`. We do the same for `h = 3` and add `papers[3] = 1` to `s`. Now `s = 3` which is not less than `h = 3`, so we stop and return `h = 3`.
+*/
